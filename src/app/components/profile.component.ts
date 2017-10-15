62a0ad15fb81a62bb4645566464acc60acf33b14
SiamKing <thesiameseelectric@gmail.com>
2017-10-15 10:12:38 -0700

    created profile components

---
+++ src/app/components/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class ProfileComponent {
+  title = 'app';
+}
